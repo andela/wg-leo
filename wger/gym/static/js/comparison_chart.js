45389b9ec04ece618484f950c181cf9c3af88c24
Jomo <jomomg@gmail.com>
2018-07-18 15:35:52 +0300

    [feature] Add code for drawing line chart

---
+++ wger/gym/static/js/comparison_chart.js
@@ -0,0 +1,82 @@
+let user_plot_data = JSON.parse(user_data.replace(/&quot;/g,'"'));
+console.log(user_plot_data);
+let user_weights = user_plot_data[0];
+let dates = user_plot_data[1];
+let plot_data = [];
+
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+Chart.defaults.line.spanGaps = true;
+
+for (let user in user_weights) {
+  color = getRandomColor();
+  if (user_weights.hasOwnProperty(user)) {
+    plot_data.push({
+      label: user,
+      data: user_weights[user],
+      lineTension: 0.3,
+      fill: false,
+      backgroundColor: color,
+      borderColor: color,
+    })
+  }
+}
+
+let chartData = {
+  labels: dates,
+  datasets: plot_data,
+};
+
+let chartOptions = {
+  legend: {
+    display: true,
+    spanGaps: true,
+    position: 'top',
+    labels: {
+      boxWidth: 80,
+      fontColor: 'black'
+    }
+  },
+  title: {
+    text: 'Gym member weight comparison'
+  },
+  scales: {
+    yAxes: [{
+      scaleLabel: {
+        display: true,
+        labelString: 'Weight in Kg',
+      }
+    }],
+    xAxes: [{
+      scaleLabel: {
+        display: true,
+        labelString: 'Date',
+      }
+    }]
+  },
+};
+
+window.onload = function () {
+  let ctx = document.getElementById("chart");
+  new Chart(ctx, {
+  type: 'line',
+  data: chartData,
+  options: chartOptions
+});
+};
+
+
+
+
+
+
+
+
+
